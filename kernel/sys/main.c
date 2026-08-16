be50f0e78e8e12c90cbe7e6abe2fd841091ad7f1
Kenraaliskuutteri <aleksi.puhakainen@gmail.com>
2026-08-16 10:02:08 +0300

    Plz work dude i beg

--- kernel/sys/main.c
+++ kernel/sys/main.c
@@ -3,25 +3,25 @@
 #include "kernel/idt.h"
 
 void kmain(void) {
-    vga_init();
-    vga_puts("Long-OS Kernel Starting...\n");
+    tty_init();
+    tty_puts("Long-OS Kernel Starting...\n");
 
-if (gdt_init() == 0) {
-    vga_puts("[OK] GDT Initialized\n");
-} else{
-    vga_puts("[CRITICAL] GDT Initialization Failed\n");
-}
+    if (gdt_init() == 0) {
+        tty_puts("[OK] GDT Initialized\n");
+    } else {
+        tty_puts("[CRITICAL] GDT Initialization Failed\n");
+    }
 
-if (idt_init() == 0) {
-    vga_puts("[  OK  ] IDT Initialized\n");
-} else {
-    vga_puts("[CRITICAL] IDT Initialization Failed\n");
-    
-    vga_puts("Ready for memory management setup.\n");
+    if (idt_init() == 0) {
+        tty_puts("[  OK  ] IDT Initialized\n");
+    } else {
+        tty_puts("[CRITICAL] IDT Initialization Failed\n");
+    }
+
+    tty_puts("Ready for memory management setup.\n");
 
     // Infinite halt loop, halting CPU to save power
     while (1) {
         __asm__ __volatile__("hlt");
     }
-}
 }
